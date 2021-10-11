c1c5fe4720fc39a559363908c3ef97aaac47478b
Mathis <Mathis.Bueckmann@uni-ol.de>
2021-10-11 16:54:23 +0200

    Initial commit

---
+++ src/main/java/com/patatamaxtex/aoe2_rule_book_website/Aoe2RuleBookWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.patatamaxtex.aoe2_rule_book_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aoe2RuleBookWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aoe2RuleBookWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/patatamaxtex/aoe2_rule_book_website/Aoe2RuleBookWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patatamaxtex.aoe2_rule_book_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aoe2RuleBookWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
